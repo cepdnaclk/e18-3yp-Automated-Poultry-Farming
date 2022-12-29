ca11990bab68a35b71445cf3181d5ca886031dd9
Shamilkhan4147 <shamilkhan414@gmail.com>
2022-12-29 16:28:39 +0530

    Refactor name

--- lib/screens/FeedAutomation/addAutomatedData.dart
+++ lib/screens/FeedAutomation/addAutomatedData.dart
@@ -17,7 +17,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 
-import 'data_display.dart';
+import 'dataDisplay.dart';
 
 class AutomatedData extends StatefulWidget {
   final String id_flock;
