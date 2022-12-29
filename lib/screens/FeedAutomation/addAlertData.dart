12c8ad50103c63d7f7286da0b4c9698803bacbce
Shamilkhan4147 <shamilkhan414@gmail.com>
2022-12-29 23:40:32 +0530

    add alert data

---
+++ lib/screens/FeedAutomation/addAlertData.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AddAlertData extends StatefulWidget {
+  const AddAlertData({Key? key}) : super(key: key);
+
+  @override
+  State<AddAlertData> createState() => _AddAlertDataState();
+}
+
+class _AddAlertDataState extends State<AddAlertData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
