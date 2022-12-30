bef111df82a213d98318367c5c9134f8a5b440a9
Shamilkhan4147 <shamilkhan414@gmail.com>
2022-12-30 17:55:18 +0530

    Changing the route of notification

--- lib/screens/FeedAutomation/addFeedData.dart
+++ lib/screens/FeedAutomation/addFeedData.dart
@@ -110,8 +110,8 @@ class _AddAutomatedFeedState extends State<AddAutomatedFeed> {
         if(payload != null && payload.isNotEmpty){
           //once the notification is clicked the person will be redirected to this page
           Navigator.push(context,MaterialPageRoute(builder: (BuildContext context){
-            return TankAlertPage();
-            //return DataDisplayPage(id_flock : widget.id_flock , startDateNavi : widget.startDateNavi, strainNavi : widget.strainNavi, info : payload.toString() );
+            //return TankAlertPage();
+            return DataDisplayPage(id_flock : widget.id_flock , startDateNavi : widget.startDateNavi, strainNavi : widget.strainNavi, info : payload.toString() );
             //id_flock: args.flockID,
             //startDateNavi: startDate,
             //strainNavi: strainType,
